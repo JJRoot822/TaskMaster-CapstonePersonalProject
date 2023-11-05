944206f0ce356199edac101ebd8546016b89f194
JJRoo822 <josh.root822@gmail.com>
2023-11-05 15:39:54 -0800

    Made some more updates to the documentation of the project.

--- TaskMaster_API/TaskMaster/Model/API/UserData/APIUser.cs
+++ TaskMaster_API/TaskMaster/Model/API/UserData/APIUser.cs
@@ -5,7 +5,7 @@ namespace TaskMaster.Model.API.UserData;
 
 public class APIUser
 {
-    public string UserId { get; set; }
+    public int UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Username { get; set; }
